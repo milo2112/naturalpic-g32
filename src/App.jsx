b983db42956140184c8660cc150a628a80ad7869
milo2112 <mjrp.ros@gmail.com>
2023-07-13 19:18:01 -0400

    aplica useEffect y setea estado

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import "./styles.css";
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-
-import Home from "./views/Home";
-import Favoritos from "./views/Favoritos";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from "./views/Home"
+import Favoritos from "./views/Favoritos"
+import { useEffect, useState } from "react"
 
 export default function App() {
-  const endpoint = "/fotos.json";
+  const endpoint = "/fotos.json"
+  const [photos, setPhotos] = useState([])
+  const getData = async (url) => {
+    const resp = await fetch(url)
+    const data = await resp.json()
 
+    setPhotos(data.photos)
+  }
+
+  useEffect(() => {
+    getData(endpoint)
+  }, [])
+
+  const sharedGlobal = { photos, setPhotos}
   return (
     <div className="App">
+      
       <BrowserRouter>
         <Navbar />
 
