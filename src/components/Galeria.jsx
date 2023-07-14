343e903e77d78de315695cb171ae09a68ca57fb9
milo2112 <mjrp.ros@gmail.com>
2023-07-13 20:39:17 -0400

    consume context para desplegar fotos

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -1,11 +1,29 @@
-import "../assets/css/galeria.css";
-import Heart from "./Heart";
+import { useContext } from "react"
+import "../assets/css/galeria.css"
+import Heart from "./Heart"
+import GlobalContext from "../context/GlobalContext"
 
-export default function Home() {
+export default function Galeria() {
+  const { photos, setPhotos } = useContext(GlobalContext)
 
+  const setFavorito = (id) => {
+    const photosIndex = photos.findIndex((f) => f.id === id)
+    photos[photosIndex].liked = !photos[photosIndex].liked
+
+    setPhotos([...photos])
+  }
   return (
     <div className="galeria grid-columns-5 p-3">
-
+      {photos.map(({ id, src, liked, alt }) => (
+        <div 
+          onClick={() => setFavorito(id)}
+          key={id}
+          className="foto" 
+          style={{ backgroundImage: `url(${src.original})` }}>
+          <Heart filled={liked} />
+          <p> {alt} </p>
+        </div>
+      ))}
     </div>
   );
 }
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,9 @@
-import Galeria from "../components/Galeria";
+import Galeria from "../components/Galeria"
 
 export default function Home() {
   return (
     <div id="Home">
       <h1>Natural Pic</h1>
-
       <Galeria />
     </div>
   );
